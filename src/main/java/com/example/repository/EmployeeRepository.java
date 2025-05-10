5f65af65dff11c3c6a7125adc1033a79333c8732
Madapada-TarunKumar-Git <tarunkumar.madapada@gmail.com>
2025-05-10 19:15:10 +0530

    EmpRepository interface created which enables to write the sql queries automatically

--- src/main/java/com/example/repository/EmployeeRepository.java
+++ src/main/java/com/example/repository/EmployeeRepository.java
@@ -1,5 +1,11 @@
 package com.example.repository;
 
-public class EmployeeRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.entity.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
 }
