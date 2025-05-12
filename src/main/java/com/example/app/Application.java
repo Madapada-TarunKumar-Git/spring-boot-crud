7423e42dd47852b531d3a99c8c1c7c49d16ddb02
Madapada-TarunKumar-Git <tarunkumar.madapada@gmail.com>
2025-05-12 23:09:10 +0530

    updates done to fix the errors

--- src/main/java/com/example/app/Application.java
+++ src/main/java/com/example/app/Application.java
@@ -2,11 +2,18 @@ package com.example.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 
 @Configuration
 @ComponentScan(basePackages = {"com.example"})
+
+@EntityScan(basePackages = "com.example.entity")
+
+@EnableJpaRepositories(basePackages = "com.example.repository")
 
 @SpringBootApplication
 public class Application {
